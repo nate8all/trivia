c9e23b4d1a4a28081b8a6c982d4cc4d7d4a2a160
nate8all <nathanalleyne@students.ptechnyc.org>
2020-01-27 18:23:32 -0500

    trying again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import Question from './Question';
 // import components
 
 class App extends Component {
-
+  constructor (props) {
+    super(props);
+    this.state = {answer1: 'Chelsea, NYC', 
+                  answer2: 'Bay Ridge, NYC', 
+                  answer3: 'Chicago, Illinois', 
+                  answer4: 'Tulsa, Oklahoma' };
+  }
   render() {
     return (
       <div className="app">
         Trivia!
-        
+        <Question></Question>
       </div>
     );
   }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { isConstructorDeclaration } from 'typescript';
 // import components
 
 
 class Question extends Component {
-  
+  constructor (props) {
+    super(props);
+  }
   render() {
+    let question = "Where is the Google Code Next Office located?";
     return (
       <div>
-        Add your QuestionText, Reset Button and AnswerButtons here.  
+        <h1>{question}</h1>
+
       </div>
     );
   }
