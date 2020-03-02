ba4afb608b66f3381f85d7453c2d0cfca7d11499
Kayla <kaylacampbell@students.ptechnyc.org>
2020-03-02 18:27:30 -0500

    changes

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { isConstructorDeclaration } from 'typescript';
+import {buildFirebase} from '../clients/firebase.js';
 // import components
 
 
 class Question extends Component {
   constructor (props) {
     super(props);
+    this.state = ""
   }
   render() {
-    let kahoot = firebase.database();
+    let kahoot = buildFirebase();
     let question = kahoot.ref("/question");
-    question.once("value").then(getQuestions);
+    question.once("value").then(function(data) {
+      const questions = data.val();
+    });
+     
     return (
       <div>
         <h1>{this.props.data.question}</h1>
       </div>
+       if (this.state.error) {
+        return <div>{this.state.errorMessage}</div>
+      } else if (this.state.imageUrl) {
+        return <Giphy imageUrl={this.state.imageUrl}/>;
+      } else {
+        return null;
+      }
     );
   }
 }
